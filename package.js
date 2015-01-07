b4fc687950a41a44324ee258c68422a890e191fe
Maxime Quandalle <maxime@quandalle.com>
2015-01-07 15:13:55 +0100

    First commit

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  summary: "Add a Meteor.isMobile boolean variable",
+  version: "0.3.6_1",
+  name: "mquandalle:jquery-textcomplete",
+  git: "https://github.com/mquandalle/meteor-jquery-textcomplete.git"
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
+  api.use("jquery", "client");
+  var basePath = "jquery-textcomplete/dist/jquery.textcomplete";
+  api.addFiles([basePath + ".js", basePath + ".css"], "client");
+});
